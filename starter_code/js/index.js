7c4be384ee4eb665dae019ff7285190d63017499
fernshin22 <ferntak22@Hotmail.com>
2017-08-16 18:39:25 -0700

    Fernanda Final Project HW

---
+++ starter_code/js/index.js
@@ -0,0 +1,23 @@
+// if you click on white button, change background to white and text to black..re
+// ifyou
+
+$(document).ready(function(){
+
+
+
+	$(document).ready(function(){
+	 $(".hamburger").click(function(){
+	 $("nav").toggleClass("open");
+	    })
+	})
+
+
+
+
+
+
+
+
+	
+       
+})
